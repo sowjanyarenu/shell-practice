c02b9171e44f1afa99bfb417d63f1a4d623bb7ec
sowjanya <pemmarajurenuka8@gmail.com>
2026-02-05 17:42:03 +0530

    message

---
+++ 07-varaibles.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#### Secial Variables ####
+
+echo "All args passed to script: $@"
+echo "Number of vars passed to script: $#"
+echo "scrit name: $0"
+echo "Present directory: $PWD"
+echo "Who is running: $USER"
+echo "Home directory of current user: $HOME"
+echo "PID of this script: $$"
+sleep 100 &
+echo "PID of recently executed backgroun proces: $!"
+
