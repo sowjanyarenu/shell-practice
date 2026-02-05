4ea89687550712ebcf3543e82c0380deb2021207
sowjanya <pemmarajurenuka8@gmail.com>
2026-02-05 08:07:10 +0530

    actions

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+ #!/bin/bash
+
+ echo "Suresh :: Hello Ramesh, how are you ?"
+ echo  "Ramesh :: Hi Suresh, I am fine thankyou. How are you ?"
+ echo  "Suresh :: Doing good, what are you learning"
+ echo  "Ramesh :: I am learning shell script, what about yougit"
