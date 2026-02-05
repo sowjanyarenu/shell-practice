7840707deaf8bb0338d93a4ad676bb032f4fd8ce
sowjanya <pemmarajurenuka8@gmail.com>
2026-02-05 08:34:01 +0530

    message

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Suresh
+PERSON2=Ramesh
+
+echo   "$Suresh :: Hello $Ramesh, how are you ?"
+ echo  "$Ramesh :: Hi $Suresh, I am fine thankyou. How are you ?"
+ echo  "$Suresh :: Doing good, what are you learning"
+ echo  "$Ramesh :: I am learning shell script, what about yougit"
