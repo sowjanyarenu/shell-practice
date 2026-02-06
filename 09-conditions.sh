d404f0874055a72d2c42326a622159ba0b5808f1
sowjanya <pemmarajurenuka8@gmail.com>
2026-02-06 12:18:02 +0530

    message

---
+++ 09-conditions.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+NUMBER=$1
+
+if [ $NUMBER -gt 20 ]; then
+    echo "Given number: $NUMBER is greater than 20"
+elif [ $NUMBER -eq 20 ]; then
+    echo "Given number: $NUMBER is equal to 20"
+else [ $NUMBER -lt 20 ]; then
+    echo "Given number: $NUMBER is less than 20"
+
+fi
